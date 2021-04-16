1cc2a2c0f048927915cd29ac9e1d496a29c26718
root <Marat R. Shakurov>
2021-04-16 20:02:31 +0000

    test script modified

--- test_check_table_count.sh
+++ test_check_table_count.sh
@@ -2,12 +2,19 @@
 
 set -x
 
-	let OSMTABCOUNT=0+$(sudo -u postgres psql -d gis -X -A -c "select count(*) from information_schema.tables where table_schema = 'public' and table_catalog = 'gis'" -q -t)
-	if ((OSMTABCOUNT==0)); then echo No OSM tables; else echo OSM tables count: $OSMTABCOUNT
-	if ((OSMTABCOUNT>0)); then
-		echo "### OSM tables already exists!"
-		echo "### To create new OSM data use environment variable \$OVERWRITEOSMDATA"
-		exit 5
-	fi
+let OSMTABCOUNT=0+$(sudo -u postgres psql -d gis -X -A -c "select count(*) from information_schema.tables where table_schema = 'public' and table_catalog = 'gis'" -q -t)
+echo $OSMTABCOUNT
 
-echo Begin import process...
+if ((OSMTABCOUNT==0)); then echo No OSM tables; else echo OSM tables count: $OSMTABCOUNT; fi
+
+if ((OSMTABCOUNT>0)); then
+    set +x >null
+    echo "@@@ OSM tables already exists!"
+    echo "@@@ To create new OSM data use environment variable \$OVERWRITEOSMDATA"
+    set -x
+    exit 5
+fi
+
+echo Begin import process
+exit 0
+
